d540eda825dbf7f3ad050bceb87d560d650f7b8a
Pietro Di Bello <pierodibello@gmail.com>
2018-10-08 21:15:11 +0200

    improve logs on HTTP request and response

--- goose-game-two/src/main/java/com/goosegame/main.java
+++ goose-game-two/src/main/java/com/goosegame/main.java
@@ -2,6 +2,9 @@ package com.goosegame;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import spark.Filter;
+import spark.Request;
+import spark.Response;
 
 import static spark.Spark.*;
 
@@ -12,9 +15,8 @@ public class main {
 
     public static void main(String[] args) {
         port(8080);
-        before((req, res) -> {
-            logger.info("URL request: {} {} - body: {}", req.requestMethod(), req.uri(), req.body());
-        });
+        before((req, res) -> logger.info("Request: {} - body: {}", req.requestMethod(), req.body()));
+        after((req, res) -> logger.info("Response body: {}", res.body()));
 
         post("/players", (req, res) -> app.createPlayer(req, res));
         post("/players/:id/roll", (req, res) -> app.roll(req, res));
